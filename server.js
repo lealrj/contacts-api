6ed3a5b53f99430c9e7a73032c0418ce2f9fd717
lealrj <derecklisboaj3@gmail.com>
2026-07-12 20:39:20 -0300

    chore: bootstrap contacts API

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+ res.send('Hello World!');
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+ console.log(`Server is running on port ${PORT}`);
+});
