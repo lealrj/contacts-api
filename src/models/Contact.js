8a1ed9373be6395496d5716703e09a33311a1f33
lealrj <derecklisboaj3@gmail.com>
2026-07-12 21:08:09 -0300

    feat: add contact model

---
+++ src/models/Contact.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Contact = sequelize.define('Contact', {
+ name: {
+  type: DataTypes.STRING,
+  allowNull: false,
+ },
+ email: {
+  type: DataTypes.STRING,
+  allowNull: false,
+  unique: true,
+  validate: {
+   isEmail: true,
+  }
+ },
+ phone: {
+  type: DataTypes.STRING,
+  allowNull: false,
+  unique: true,
+  validate: {
+   is: /^[0-9]{10}$/,
+  }
+ },
+ favorite: {
+  type: DataTypes.BOOLEAN,
+  allowNull: false,
+ },
+});
