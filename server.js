b56b6175472c12b2edb922d2123ea55f971337bf
lealrj <derecklisboaj3@gmail.com>
2026-07-13 21:36:16 -0300

    feat: add server.js with Express and Sequelize setup
    Configure middlewares, routes and database sync on startup

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
-require('dotenv').config();
 const express = require('express');
+const sequelize = require('./src/config/database');
+const contactRoutes = require('./src/routes/contacts');
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
- res.send('Hello World!');
+app.use('/api', contactRoutes);
+
+app.use((err, req, res, next) => {
+ console.error(err.stack);
+ res.status(500).json({
+  error: 'Internal server error'
+ });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}`);
-});
+
+async function start() {
+ try {
+  await sequelize.authenticate();
+  console.log('Db Connected')
+
+  await sequelize.sync({ alter: true });
+  console.log('Models Sync')
+
+  app.listen(PORT, () => {
+   console.log(`Server running on port: ${PORT}`)
+  });
+ } catch (error) {
+  console.error('Unable to connect to the database:', error);
+ }
+}
+
+start();
